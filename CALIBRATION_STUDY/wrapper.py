975398b502b5d2c96e09f8551fc2416225bb9ff8
Kirstie Whitaker <kw401@cam.ac.uk>
2013-07-18 13:50:36 +0100

    updated

--- CALIBRATION_STUDY/wrapper.py
+++ CALIBRATION_STUDY/wrapper.py
@@ -41,29 +41,13 @@ sep_av = [ 'SEPARATE' ]
 transform = [ 'MNI_FNIRT_MPRAGE_BBR_B0' ]
 roi_name = [ 'lcing', 'rcing', 'wholebrain', 'bodycc' ]
 
-# The FA, MD and volume data are all in files created in the appropriate directories deep in the data_dir.
-# So now we're going to put them all together.
-
-# Define your results_dir. This folder replicates a lot of the directory structure in the subject dirs in a directory called "RESULTS" in the data_dir.
-# Make the results_dir if it doesn't already exist.
-# Example results_dir: RESULTS/EXCL_VOL31/B0S_6/B0_ORDER_00_14_27_41_54_68/SEPARATE/MNI_FNIRT_MPRAGE_BBR_B0
-results_dir = os.path.join(data_dir, 'RESULTS', incl_excl, 'B0S_{}'.format(n_b0s),
-                            'B0_ORDER_{}'.format(b0_order), sep_av, transform)
-
-
-# Make the directory if it doesn't yet exist
-if not os.path.exists(results_dir):
-    os.makedirs(results_dir)
-
-    # Define the results file
-# This is specific to the ROI name
-# Example results_file: bodycc_FA_MD_vol.txt
-results_file = os.path.join(results_dir, '{}_FA_MD_vol.txt'.format(roi_name))
-                
-
 for incl_excl, n_b0s, ec_b0, b0_order, sep_av, transform, roi_name in it.product(incl_excl, n_b0s, ec_b0, b0_order, sep_av, transform, roi_name):
     
     results_file, results_dir = wrangle_text_files(incl_excl, n_b0s, ec_b0, b0_order, sep_av, transform, roi_name)
+    
+    data = read_in_data(results_file)
+    
+    colors, shapes = set_colors_shapes()
     
     plot_data(results_file, results_dir)
 
