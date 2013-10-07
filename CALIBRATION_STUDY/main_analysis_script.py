204be845c187bb449c6c4ec9cfe82f3baf89fab0
Kirstie Whitaker <kw401@cam.ac.uk>
2013-10-07 20:12:40 +0100

    Including registrations

--- CALIBRATION_STUDY/main_analysis_script.py
+++ CALIBRATION_STUDY/main_analysis_script.py
@@ -25,29 +25,29 @@ data_dir = '/work/imagingD/NSPN/workspaces/kw401/BOOTSTRAPPING_ANALYSES'
 #==============================================================================
 # Define some variables
 #  subs: subject ids
-
+#
 #  locs: scanner locations
-
+#
 #  scans: which of the DTI scans you're using
 #       (This only applies to the UCL data where there are two.
 #       At all other sites there is only one.)
-
+#
 #  incl_excl: either the original data or data excluding volume 31
 #       which is not the same at CBSU as it is at the other sites
-
+#
 #  n_b0s: number of b0s used to calculate tensor
-
+#
 #  ec_b0: the b0 that was used as the eddy_correct target volume
-
+#
 #  b0_order: the order that the b0s were in the 4D file
-
+#
 #  sep_av: whether the b0s were averaged before the tensor was fit
 #       or kept separate
-
+#
 #  transform: the registration path from MNI to DTI space
-
+#
 #  roi_name: the name of the roi
-#==============================================================================
+#------------------------------------------------------------------------------
 subs = [ '06', '07', '08' ]
 locs = [ 'WBIC', 'CBSU', 'UCL' ]
 scans = [ 'DTI_2A', 'DTI_2B' ]
@@ -64,6 +64,15 @@ roi_name = [ 'lcing', 'rcing', 'wholebrain', 'bodycc' ]
 # And now get going:
 
 for incl_excl, n_b0s, ec_b0, b0_order, sep_av, transform, roi_name in it.product(incl_excl, n_b0s, ec_b0, b0_order, sep_av, transform, roi_name):
+    '''
+    Loop through all the combinations of include/exclude volume 31,
+        the number of b0s,
+        which b0 was used as the eddy correct volume, 
+        whether the b0s were combined first or not,
+        how the roi was transformed from standard space,
+        and all the rois
+    '''
+    run_registrations(data_dir, incl_excl, n_b0s, b0_order, sep_av, subs, locs, scans)
     
     results_file, results_dir = wrangle_text_files(data_dir, incl_excl, n_b0s,
                                         ec_b0, b0_order, sep_av, transform,
