5e95d60b65ee19b76f3d6d6c9d63cdc80793e1a8
Kirstie Whitaker <kw401@cam.ac.uk>
2013-10-09 13:26:47 +0100

    more print to screen

--- CALIBRATION_STUDY/plot_data.py
+++ CALIBRATION_STUDY/plot_data.py
@@ -21,31 +21,35 @@ def plot_data(data, results_dir, roi_name, colors, shapes):
     from plot_by_locs import plot_by_locs
     #==========================================================================
     
-    print '  Plotting data'
-    
-    # Get a list of the subjects:
-    subs = [ sub for sub in data['sub'] ]
-    subs = list(set(subs))
-    sub_ids = [ np.int(sub) for sub in subs ]
-
-    # List of location_ids
-    loc_ids = [ loc_id for loc_id in data['loc_id'] ]
-    loc_ids = list(set(loc_ids))
-    loc_ids = [ np.int(loc_id) for loc_id in loc_ids ]
-
-    # List of locations
-    locs = [ data['loc'][data['loc_id']==loc_id][0] for loc_id in loc_ids ]
-    
-    output_name = os.path.join(results_dir, '{}_plot_by_subs.png'.format(roi_name))
-    
-    plot_by_subs(data=data, output_name=output_name,
-                            colors=colors, shapes=shapes,
-                            sub_ids=sub_ids, loc_ids=loc_ids, roi_name=roi_name, figsize=(15,5))
-    
     output_name = os.path.join(results_dir, '{}_plot_by_locs.png'.format(roi_name))
-    
-    plot_by_locs(data=data, output_name=output_name,
-                            colors=colors, shapes=shapes,
-                            sub_ids= sub_ids, loc_ids=loc_ids, locs=locs,
-                            roi_name=roi_name, figsize=(15,5))
 
+    if not os.path.exists(output_name):
+        print '  Plotting data'
+    
+        # Get a list of the subjects:
+        subs = [ sub for sub in data['sub'] ]
+        subs = list(set(subs))
+        sub_ids = [ np.int(sub) for sub in subs ]
+
+        # List of location_ids
+        loc_ids = [ loc_id for loc_id in data['loc_id'] ]
+        loc_ids = list(set(loc_ids))
+        loc_ids = [ np.int(loc_id) for loc_id in loc_ids ]
+
+        # List of locations
+        locs = [ data['loc'][data['loc_id']==loc_id][0] for loc_id in loc_ids ]
+        
+        output_name = os.path.join(results_dir, '{}_plot_by_subs.png'.format(roi_name))
+        
+        plot_by_subs(data=data, output_name=output_name,
+                                colors=colors, shapes=shapes,
+                                sub_ids=sub_ids, loc_ids=loc_ids, roi_name=roi_name, figsize=(15,5))
+        
+        output_name = os.path.join(results_dir, '{}_plot_by_locs.png'.format(roi_name))
+        
+        plot_by_locs(data=data, output_name=output_name,
+                                colors=colors, shapes=shapes,
+                                sub_ids= sub_ids, loc_ids=loc_ids, locs=locs,
+                                roi_name=roi_name, figsize=(15,5))
+    else:
+        print '  Data already plotted'
