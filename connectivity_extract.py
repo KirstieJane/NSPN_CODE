b01ef7d0c0cbbdebadba8a34fd759efd5c3ed174
Kirstie Whitaker <kw401@cam.ac.uk>
2014-06-24 20:13:14 +0100

    added path and commented out old stuff

--- connectivity_extract.py
+++ connectivity_extract.py
@@ -4,6 +4,8 @@ import scipy.io as sio
 from glob import glob
 
 data_dir = glob(os.path.join('/work/imaging*/NSPN/workspaces/kw401/UCHANGE/INTERIM_ANALYSIS'))[0]
+
+'''
 mat_files = glob(os.path.join(data_dir, 'SUB_DATA', '*', 'DTI','MRI0','CONNECTIVITY','*mat'))
 
 
@@ -16,8 +18,9 @@ for mat_file in mat_files:
     for m in [ 'cost', 'k', 'a', 'arand', 'M', 'Mrand', 'C', 'Crand', 'L', 'Lrand', 'Sigma', 'E', 'Erand', 'CE', 'CErand' ]:
         print "{}: {}".format(m, meas['Meas_'+subid][m][0][0][0][0])
         
-        
-mat = loadmat('Meas_network_measures.mat') # Load in the .mat file
+'''
+       
+mat = loadmat(os.path.join(data_dir, 'SUB_DATA', 'Meas_network_measures.mat')) # Load in the .mat file
 mdata = mat['Meas_network_measures']  # variable of interest in mat file
 mdtype = mdata.dtype  # dtypes of structures are "unsized objects"
 ndata = {n: mdata[n][0, 0] for n in mdtype.names} # get all the data from mdata
