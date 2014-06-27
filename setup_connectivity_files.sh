11f2ba541cd6a039ab02706a6a98851dde979a5f
Kirstie Whitaker <kw401@cam.ac.uk>
2014-06-27 16:10:04 +0100

    ensure that parcellations are consecutive

--- setup_connectivity_files.sh
+++ setup_connectivity_files.sh
@@ -13,15 +13,10 @@ surfer_dir=$4
 
 parcellation_to_dti_transform=${reg_dir}/freesurfer_TO_diffB0_BBR.mat
 
-### NOTE: THIS BOUNDARY FILE ONLY WORKS FOR THE 500 PARCELLATION! ###
-# HARD CODED - MAY NEED TO CHANGE IN THE FUTURE #
-sub_cortex_boundary=(`cat ${surfer_dir}/parcellation/boundary_sub_lh.txt`)
-
 parcellation_file_DTIspace=${dti_dir}/CONNECTIVITY/`basename ${parcellation_file} .nii.gz`_DTIspace.nii.gz
-parcellation_file_cortexonly_DTIspace=${dti_dir}/CONNECTIVITY/`basename ${parcellation_file} .nii.gz`_cortexonly_DTIspace.nii.gz
 white_matter_file=${surfer_dir}/mri/wm.nii
 white_matter_file_DTIspace=${dti_dir}/CONNECTIVITY/`basename ${white_matter_file} .nii`_DTIspace.nii.gz
-parcellation_file_cortexANDwhitematteronly_DTIspace=${dti_dir}/CONNECTIVITY/`basename ${parcellation_file} .nii.gz`_cortexANDwhitematteronly_DTIspace.nii.gz
+parcellation_file_WhiteMatterUnderCortex_DTIspace=${dti_dir}/CONNECTIVITY/`basename ${parcellation_file} .nii.gz`_WhiteMatterUnderCortex_DTIspace.nii.gz
 
 # RUN THE CODE
 # Make the connectivity directory if it doesn't already exist
@@ -37,11 +32,6 @@ if [[ ! -f ${parcellation_file_DTIspace} ]]; then
             -interp nearestneighbour
 fi
 
-# Remove subcortical regions if the cortexonly file doesn't yet exist
-if [[ ! -f ${parcellation_file_cortexonly_DTIspace} ]]; then
-    fslmaths ${parcellation_file_DTIspace} -sub ${sub_cortex_boundary} -add 1 -thr 0 ${parcellation_file_cortexonly_DTIspace}
-fi        
-
 # Flirt the white matter mask to DTI space if it doesn't yet exist
 if [[ ! -f ${white_matter_file_DTIspace} ]]; then
     flirt -in ${white_matter_file} \
@@ -53,6 +43,6 @@ if [[ ! -f ${white_matter_file_DTIspace} ]]; then
 fi
 
 # Create the intersection file of the parcellation and white matter if it doesn't yet exist
-if [[ ! -f ${parcellation_file_cortexANDwhitematteronly_DTIspace} ]]; then
-    fslmaths ${white_matter_file_DTIspace} -bin -mul ${parcellation_file_cortexonly_DTIspace} ${parcellation_file_cortexANDwhitematteronly_DTIspace}
+if [[ ! -f ${parcellation_file_WhiteMatterUnderCortex_DTIspace} ]]; then
+    fslmaths ${white_matter_file_DTIspace} -bin -mul ${parcellation_file_DTIspace} ${parcellation_file_WhiteMatterUnderCortex_DTIspace}
 fi
