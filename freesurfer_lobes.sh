639790c4bb6d7a58e95a7a0239377fa2125b3a6d
Kirstie Whitaker <kw401@cam.ac.uk>
2014-07-22 16:17:47 +0100

    added some useful if statements
    Stops the code from running if it is already complete!

--- freesurfer_lobes.sh
+++ freesurfer_lobes.sh
@@ -8,27 +8,35 @@ surf_sub=`basename ${surfer_dir}`
 # First you have to pull together the labels
 # from the surface annotation files
 for hemi in lh rh; do
-    mri_annotation2label --subject ${surf_sub} \
-                         --hemi ${hemi} \
-                         --lobesStrict \
-                         ${surfer_dir}/label/${hemi}.lobesStrict
+    if [[ ! -f ${surfer_dir}/label/${hemi}.lobesStrict ]]; then
+        mri_annotation2label --subject ${surf_sub} \
+                             --hemi ${hemi} \
+                             --lobesStrict \
+                             ${surfer_dir}/label/${hemi}.lobesStrict
+    fi
 done
+
 
 # Transform the surface annotation into a segmentation volume
 # and label the white matter up to 5mm beneath the lobes
-mri_aparc2aseg --s ${surf_sub} \
-            --labelwm \
-            --rip-unknown \
-            --annot lobesStrict \
-            --o ${surfer_dir}/mri/lobes+aseg.mgz
+if [[ ! -f ${surfer_dir}/mri/lobes+aseg.mgz ]]; then
+
+    mri_aparc2aseg --s ${surf_sub} \
+                --labelwm \
+                --rip-unknown \
+                --annot lobesStrict \
+                --o ${surfer_dir}/mri/lobes+aseg.mgz
+fi
 
 # Extract some statistics :)
-mri_segstats --i ${surfer_dir}/mri/norm.mgz \
-             --seg ${surfer_dir}/mri/lobes+aseg.mgz \
-             --sum ${surfer_dir}/stats/lobes+aseg.stats \
-             --pv ${surfer_dir}/mri/norm.mgz \
-             --id 1001 1003 1004 1005 1006 1007 \
-                  2001 2003 2004 2005 2006 2007 \
-                  3001 3003 3004 3005 3006 3007 \
-                  4001 4003 4004 4005 4006 4007
+if [[ ! -f ${surfer_dir}/stats/lobes+aseg.stats ]]; then
+    mri_segstats --i ${surfer_dir}/mri/norm.mgz \
+                 --seg ${surfer_dir}/mri/lobes+aseg.mgz \
+                 --sum ${surfer_dir}/stats/lobes+aseg.stats \
+                 --pv ${surfer_dir}/mri/norm.mgz \
+                 --id 1001 1003 1004 1005 1006 1007 \
+                      2001 2003 2004 2005 2006 2007 \
+                      3001 3003 3004 3005 3006 3007 \
+                      4001 4003 4004 4005 4006 4007
+fi
 
