003b51d393018143deec5d9dc25fe0bd7bc682fc
Kirstie Whitaker <kw401@cam.ac.uk>
2014-07-22 12:01:22 +0100

    took out dmax value - looks ridiculous

--- freesurfer_lobes.sh
+++ freesurfer_lobes.sh
@@ -5,21 +5,31 @@ surfer_dir=$1
 SUBJECTS_DIR=${surfer_dir}/../
 surf_sub=`basename ${surfer_dir}`
 
+# First you have to pull together the labels
+# from the surface annotation files
+for hemi in lh rh; do
+    mri_annotation2label --subject ${surf_sub} \
+                         --hemi ${hemi} \
+                         --lobesStrict \
+                         ${surfer_dir}/label/${hemi}.lobesStrict
+done
+
 # Try a few different dmax values just to see
 # how that value changes the results
-for dmax in 5 10 15 20 100; do
-    mri_aparc2aseg --s ${surf_sub} \
-                --labelwm \
-                --wmparc-dmax 100 \
-                --rip-unknown \
-                --annot lobes_${dmax100}
+mri_aparc2aseg --s ${surf_sub} \
+            --labelwm \
+            --rip-unknown \
+            --annot lobesStrict \
+            --o ${surfer_dir}/mri/lobes+aseg.mgz
 
-    mri_segstats --i ${surfer_dir}/mri/norm.mgz \
-                 --seg ${surfer_dir}/mri/lobes_${dmax}+aseg.mgz \
-                 --sum ${surfer_dir}/stats/lobes_${dmax}+aseg.stats \
-                 --pv ${surfer_dir}/mri/norm.mgz
-
-done
+mri_segstats --i ${surfer_dir}/mri/norm.mgz \
+             --seg ${surfer_dir}/mri/lobes+aseg.mgz \
+             --sum ${surfer_dir}/stats/lobes+aseg.stats \
+             --pv ${surfer_dir}/mri/norm.mgz \
+             --id 1001 1003 1004 1005 1006 1007 \
+                  2001 2003 2004 2005 2006 2007 \
+                  3001 3003 3004 3005 3006 3007 \
+                  4001 4003 4004 4005 4006 4007
 
 
 #===================================================================================
