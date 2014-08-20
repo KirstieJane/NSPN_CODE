6a978c23e7db3710cc274de750aa843ab354922a
Kirstie Whitaker <kw401@cam.ac.uk>
2014-08-20 11:21:32 +0100

    don't worry about occ

--- freesurfer_rois_behavmerge.py
+++ freesurfer_rois_behavmerge.py
@@ -27,9 +27,8 @@ df_behav = pd.read_csv(behav_file)
 
 for f in file_list:
     df_meas = pd.read_csv(f)
-    if not df_meas.columns[1] == 'occ':
-        df = df_behav.merge(df_meas, on='nspn_id')
-        df.sort('nspn_id', inplace=True)
-        c_drop = [ x for x in df.columns if 'Measure' in x ][0]
-        df.drop(c_drop, inplace=True, axis=1)
-        df.to_csv(f.replace('.csv', '_behavmerge.csv'))
+    df = df_behav.merge(df_meas, on=['nspn_id', 'occ'])
+    df.sort('nspn_id', inplace=True)
+    c_drop = [ x for x in df.columns if 'Measure' in x ][0]
+    df.drop(c_drop, inplace=True, axis=1)
+    df.to_csv(f.replace('.csv', '_behavmerge.csv'))
