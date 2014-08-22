b0dba773473faf9374a0a38f36eb4158505ef8d1
Kirstie Whitaker <kw401@cam.ac.uk>
2014-08-22 10:36:44 +0100

    make a new folder for the labels etc

--- freesurfer_probtrackx.sh
+++ freesurfer_probtrackx.sh
@@ -7,31 +7,41 @@ for sub in `cat ${data_dir}/sublist`; do
 
     export SUBJECTS_DIR=`dirname ${surfer_dir}`
 
+    mkdir -p ${surfer_dir}/probtrackx/seeds_targets/
+    rm -rf ${surfer_dir}/probtrackx/targets.txt
+    
+    label_list=(`dirname ${0}`/destrieux_labels_order.txt)
+    labels=(`cat ${label_list}`)
     
     # CREATE INDIVIDUAL LABELS FOR THE ANNOTATION FILE
     # In this example we're looking at the aparc.a2009s scheme
     for hemi in lh rh; do
-        if [[ ! -f ${SUBJECTS_DIR}/MRI0/label/${hemi}.Pole_occipital.label ]]; then
-            mri_annotation2label --subject MRI0 \
-                                    --hemi ${hemi} \
-                                    --annotation ${SUBJECTS_DIR}/MRI0/label/${hemi}.aparc.a2009s.annot \
-                                    --outdir ${SUBJECTS_DIR}/MRI0/label \
-                                    --surface white                             
-        fi
-        
-        if [[ ! -f ${SUBJECTS_DIR}/MRI0/label/${hemi}.Pole_occipital.gii ]]; then
-            for label in `ls -d ${SUBJECTS_DIR}/MRI0/label/${hemi}.*label`; do
-                vol=${label/%.label/.gii}
+        for label in ${labels[@]}
+            if [[ ! -f ${surfer_dir}/probtrackx/seeds_targets/${hemi}.${label}.label ]]; then
+                mri_annotation2label --subject MRI0 \
+                                        --hemi ${hemi} \
+                                        --annotation ${surfer_dir}/label/${hemi}.aparc.a2009s.annot \
+                                        --outdir ${surfer_dir}/probtrackx/seeds_targets/ \
+                                        --surface white
+            fi
+            
+            if [[ ! -f ${surfer_dir}/probtrackx/seeds_targets/${hemi}.${label}.gii ]]; then
                 echo "converting $label to $vol"
-                mri_label2vol --label ${label} \
-                                --temp ${SUBJECTS_DIR}/MRI0/dlabel/anat_brain_mask.nii.gz \
-                                --o ${vol} \
+                mri_label2vol --label ${surfer_dir}/probtrackx/seeds_targets/${hemi}.${label}.label \
+                                --temp ${surfer_dir}/dlabel/anat_brain_mask.nii.gz \
+                                --o ${surfer_dir}/probtrackx/seeds_targets/${hemi}.${label}.gii \
                                 --identity \
                                 --fillthresh 0.5
-            done
-        fi
-    done
+                                
+                mri_label2vol --label ${surfer_dir}/probtrackx/seeds_targets/${hemi}.${label}.label \
+                                --temp ${surfer_dir}/dlabel/anat_brain_mask.nii.gz \
+                                --o ${surfer_dir}/probtrackx/seeds_targets/${hemi}.${label}.nii.gz \
+                                --identity \
+                                --fillthresh 0.5
+            fi
+        done # Close label list
+    done # Close hemi loop
     
-done
+done # Close sub loop
 
 
