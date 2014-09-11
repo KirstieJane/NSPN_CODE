2ccf55cff147ee42dd010079ce202482c112a764
Kirstie Whitaker <kw401@cam.ac.uk>
2014-09-11 11:06:30 +0100

    Big overhaul of pysurfer_plot_surface_values

--- pysurfer_plot_surface_values.py
+++ pysurfer_plot_surface_values.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
-# NEEDS SOME TIDYING BUT IT WORKS!
+#=============================================================================
+# Created by Kirstie Whitaker
+# September 2014
+# Contact: kw401@cam.ac.uk
+#=============================================================================
 
+#=============================================================================
+# IMPORTS
+#=============================================================================
 import os
+import sys
+import argparse
 import numpy as np
 import scipy.io as sio
 from glob import glob
-import pandas as pd
 
+import pandas as pd
 import nibabel as nib
 from surfer import Brain
 
@@ -19,24 +28,127 @@ import matplotlib.image as mpimg
 import matplotlib.gridspec as gridspec
 
 
-def combine_pngs(measure, surface, stat, fs_rois_dir):
+#=============================================================================
+# FUNCTIONS
+#=============================================================================
+def setup_argparser():
+    '''
+    Code to read in arguments from the command line
+    Aso allows you to change some settings
+    '''
+    # Build a basic parser.
+    help_text = ('Plot values on a freesurfer surface')
+    
+    sign_off = 'Author: Kirstie Whitaker <kw401@cam.ac.uk>'
+    
+    parser = argparse.ArgumentParser(description=help_text, epilog=sign_off)
+    
+    # Now add the arguments
+    # Required argument: dti_dir
+    parser.add_argument(dest='data_dir', 
+                            type=str,
+                            metavar='data_dir',
+                            help='data directory')
+                          
+    parser.add_argument(dest='measure', 
+                            type=str,
+                            metavar='measure',
+                            help='Measure eg: MT_mean, FA_r etc')
+                            
+    parser.add_argument(dest='roi_file', 
+                            type=str,
+                            metavar='roi_file',
+                            help='roi file containing list of measure values - one for each region - csv format')
+                            
+    parser.add_argument('--fsaverageid',
+                            type=str,
+                            metavar='fsaverage_id',
+                            help='FSaverage subject id',
+                            default='fsaverageSubP')
+                            
+    parser.add_argument('-l', '--lower',
+                            type=float,
+                            metavar='lowerthr',
+                            help='lower limit for colorbar',
+                            default=None)
+                            
+    parser.add_argument('-u', '--upper',
+                            type=float,
+                            metavar='upperthr',
+                            help='upper limit for colorbar',
+                            default=None)
+                            
+    parser.add_argument('-c', '--cmap',
+                            type=str,
+                            metavar='cmap',
+                            help='colormap',
+                            default='RdBu')
+    
+    arguments = parser.parse_args()
+    
+    return arguments, parser
 
+
+#------------------------------------------------------------------------------
+def plot_surface(vtx_data, subject_id, hemi, surface, subjects_dir, output_dir, prefix, l, u, cmap):
+    # Open up a brain in pysurfer
+    brain = Brain(subject_id, hemi, surface,
+                  subjects_dir = subjects_dir,
+                  config_opts=dict(background="white"))
+
+    # Figure out the min and max for the color bar
+    if l == None:
+        l = roi_data[roi_data>-99].min()
+    if u == None:
+        u = roi_data[roi_data>-99].max()
+        
+    l = np.floor(l*20)/20.0
+    u = np.ceil(u*20)/20.0
+    
+    # Make sure the colorbar is centered
+    if l**2 < u **2:
+        l = u*-1
+    else:
+        u = l*-1
+    
+    # Add your data to the brain
+    brain.add_data(vtx_data,
+                    l, 
+                    u,
+                    thresh = -98,
+                    colormap=cmap,
+                    alpha=.8)
+    
+    # Save the images for medial and lateral
+    # putting a color bar on all of them
+    brain.save_imageset(prefix = os.path.join(output_dir, prefix),
+                        views = views_list, 
+                        colorbar = range(len(views_list)) )
+
+#----------------------------------------------------------------------------- 
+def combine_pngs(measure, surface, output_dir):
+    '''
+    Find four images and combine them into one nice picture
+    '''
     figsize = (4.5,4)
     fig = plt.figure(figsize = figsize, facecolor='white')
 
     grid = gridspec.GridSpec(2, 2)
     grid.update(left=0, right=1, top=1, bottom = 0.08, wspace=0, hspace=0)
 
-    f_list = [ '_'.join([os.path.join(fs_rois_dir, measure), 'lh', surface, stat,       'lateral.png']),
-                '_'.join([os.path.join(fs_rois_dir, measure), 'rh', surface, stat, 'lateral.png']),
-                '_'.join([os.path.join(fs_rois_dir, measure), 'lh', surface, stat, 'medial.png']),
-                '_'.join([os.path.join(fs_rois_dir, measure), 'rh', surface, stat, 'medial.png']) ]
+    f_list = [ '_'.join([os.path.join(output_dir, measure), 'lh', surface, 'lateral.png']),
+               '_'.join([os.path.join(output_dir, measure), 'rh', surface, 'lateral.png']),
+               '_'.join([os.path.join(output_dir, measure), 'lh', surface, 'medial.png']),
+               '_'.join([os.path.join(output_dir, measure), 'rh', surface, 'medial.png']) ]
 
-                           
+    # Plot each figure in turn
     for g_loc, f in zip(grid, f_list):
         ax = plt.Subplot(fig, g_loc)
         fig.add_subplot(ax)
         img = mpimg.imread(f)
+        # Crop the figures appropriately
+        # NOTE: this can change depending on which system you've made the 
+        # images on originally - it's a bug that needs to be sorted out!
         if 'lateral' in f:
             img_cropped = img[100:700,:,:]
         else:
@@ -44,6 +156,8 @@ def combine_pngs(measure, surface, stat, fs_rois_dir):
         ax.imshow(img_cropped, interpolation='none')
         ax.set_axis_off()
 
+    # Add the bottom of one of the images as the color bar
+    # at the bottom of the combo figure
     grid_cbar = gridspec.GridSpec(1,1)
     grid_cbar.update(left=0, right=1, top=0.08, bottom=0, wspace=0, hspace=0)
     ax = plt.Subplot(fig, grid_cbar[0])
@@ -53,217 +167,84 @@ def combine_pngs(measure, surface, stat, fs_rois_dir):
     ax.imshow(img_cbar, interpolation='none')
     ax.set_axis_off()
     
+    # Save the figure
     filename = os.path.join(fs_rois_dir, '{}_{}_{}_combined.png'.format(measure, surface,stat))
     print filename
     fig.savefig(filename, bbox_inches=0, dpi=300)
 
 
+#=============================================================================
+# SET SOME VARIABLES
+#=============================================================================
+# Read in the arguments from argparse
+arguments, parser = setup_argparser()
 
+data_dir = arguments.data_dir
+measure = arguments.measure
+subject_id = arguments.fsaverageid
+roi_data_file = arguments.roi_file
+l = arguments.lower
+u = arguments.upper
+cmap = arguments.cmap
 
-data_dir = os.path.join('/work','imagingG', 'NSPN', 'workspaces', 'kw401', 'UCHANGE','INTERIM_ANALYSIS')
 subjects_dir = os.path.join(data_dir ,'SUB_DATA')
 fs_rois_dir = os.path.join(data_dir, 'FS_ROIS')
 
-subject_id = "fsaverageSubP"
 hemi_list = [ "lh", "rh" ]
 surface_list = [ "inflated", "pial" ]
+views_list = [ 'medial', 'lateral' ]
 
 seg = '500cortConsec'
+
+# Check that the inputs exist:
+if not os.path.isdir(data_dir):
+    print "Data directory doesn't exist"
+    sys.exit()
     
-if not os.path.isdir(os.path.join(fs_rois_dir, seg)):
-    os.mkdir(os.path.join(fs_rois_dir, seg))
 
-
-
-measure_list = [ 'FA', 'volume', 'MD', 'MT' ]
-
-for hemi, surface, measure in it.product(hemi_list, surface_list, measure_list):
-
-    print hemi, surface, measure
+#=============================================================================
+# READ IN THE MEASURE DATA
+#=============================================================================
+roi_data = np.loadtxt(roi_data_file)
     
-    """
-    Read in the standard parcellation
-    Of course you'll have to change this when you're looping through different files!
-    """
+output_dir = os.path.join(fs_rois_dir, seg)
+if not os.path.isdir(output_dir):
+    os.mkdir(output_dir)
+
+for hemi, surface in it.product(hemi_list, surface_list):
+
+    prefix = '_'.join([measure, hemi, surface])
+    
+    # Read in aparc annot file
     aparc_file = os.path.join(subjects_dir,
-                              subject_id, "label",
-                              hemi + ".500.aparc.annot")
-                              
+                          subject_id, "label",
+                          hemi + ".500.aparc.annot")
+
+    # Read in aparc names file
     aparc_names_file =  os.path.join(subjects_dir,
                               subject_id, "parcellation",
                               "500.names.txt")
                               
-    wm_names = [line.strip() for line in open(aparc_names_file)]
-    wm_names = wm_names[41::]
+    # Read in the names from the aparc names file 
+    # dropping the first 41 ################# BUUUUUG - needs to be fixed
+    aparc_names = [line.strip() for line in open(aparc_names_file)]
+    aparc_names = aparc_names[41::]
+    
+    # Use nibabel to merge together the aparc_names and the aparc_file
     labels, ctab, names = nib.freesurfer.read_annot(aparc_file)
-    
-    """
-    Read in the data
-    """
-    data_file = os.path.join(fs_rois_dir, 
-                                measure + '_500cortConsec_mean_behavmerge.csv')
-    if measure == 'volume':
-        data_file = os.path.join(fs_rois_dir, 
-                                '500cortConsec_volume_behavmerge.csv')
-    df = pd.read_csv(data_file)
 
+    # Make a vector containing the data point at each vertex.
+    vtx_data = roi_data[labels]
     
-    
-    # RENAME THE COLUMNS SO THEY MATCH THE NAMES IN THE APARC FILE
-    # NOTE: this really should be put into the behav merge code
-    # rather than here!
-    colnames = df.columns
-    seg_cols = [ x for x in colnames if 'Seg' in x ]
-    
-    for i, wm_name in enumerate(wm_names):
-        df[wm_name] = df[seg_cols[i+1]]
+    # Show this data on a brain
+    plot_surface(vtx_data, subject_id, hemi,
+                     surface, subjects_dir, 
+                     output_dir, prefix,
+                     l, u, cmap)
 
-    """
-    Fill in the data for each region on the surface
-    """
-    roi_data_mean = np.ones(len(names))*-99
-    roi_data_std = np.ones(len(names))*-99
-    roi_data_r = np.ones(len(names))*-99
-    roi_data_p = np.ones(len(names))*-99
-    roi_data_m = np.ones(len(names))*-99
-        
-    for i, name in enumerate(names):
-        #wm_name = 'wm-' + hemi + '-' + name
-        wm_name = '{}_{}'.format(hemi, name)
- 
-        if wm_name in df.columns:
-            roi_data_mean[i] = df[wm_name].mean()
-            roi_data_std[i] = df[wm_name].std()
-            m, c, r, p, sterr = linregress(df['HQP1_mfqtot3'], df[wm_name])
-            roi_data_m[i] = m
-            roi_data_r[i] = r
-            roi_data_p[i] = 1 - p
-
-    
-    """
-    Make a vector containing the data point at each vertex.
-    """
-    vtx_data_mean = roi_data_mean[labels]
-    vtx_data_std = roi_data_std[labels]
-    vtx_data_r = roi_data_r[labels]
-    vtx_data_p = roi_data_p[labels]
-    vtx_data_m = roi_data_m[labels]
-
-    """
-    Display these values on the brain.
-    """
-    '''
-    ### MEAN
-    brain = Brain(subject_id, hemi, surface,
-                  subjects_dir = subjects_dir,
-                  config_opts=dict(background="white"))
-
-    l = roi_data_mean[roi_data_mean>-99].min()
-    u = roi_data_mean[roi_data_mean>-99].max()
-    l = np.floor(l*20)/20.0
-    u = np.ceil(u*20)/20.0
-    
-    brain.add_data(vtx_data_mean,
-                    1.9, 
-                    3.7,
-                    thresh = -98,
-                    colormap="jet",
-                    alpha=.8)
-    
-    views_list = [ 'medial', 'lateral' ]
-    prefix = '_'.join([measure, hemi, surface, 'mean'])
-    brain.save_imageset(prefix = os.path.join(fs_rois_dir, prefix),
-                        views = views_list, 
-                        colorbar = range(len(views_list)) )
-                        
-    '''
-    
-    ### PEARSON CORR w AGE
-    brain = Brain(subject_id, hemi, surface,
-                  subjects_dir = subjects_dir,
-                  config_opts=dict(background="white"))
-
-    l = roi_data_r[roi_data_mean>-99].min()
-    u = roi_data_r[roi_data_mean>-99].max()
-    l = np.floor(l*20)/20.0
-    u = np.ceil(u*20)/20.0
-    
-    # Make sure the colorbar is centered
-    if l**2 < u **2:
-        l = u*-1
-    else:
-        u = l*-1
-    
-    brain.add_data(vtx_data_r,
-                    -0.35, 
-                    0.35,
-                    thresh = -98,
-                    colormap="RdBu_r",
-                    alpha=.8)
-    
-    views_list = [ 'medial', 'lateral' ]
-    prefix = '_'.join([measure, hemi, surface, 'r'])
-    brain.save_imageset(prefix = os.path.join(fs_rois_dir, seg, prefix),
-                        views = views_list, 
-                        colorbar = range(len(views_list)) )
-                        
-    '''
-    ### SLOPE w AGE
-    brain = Brain(subject_id, hemi, surface,
-                  subjects_dir = subjects_dir,
-                  config_opts=dict(background="white"))
-
-    l = roi_data_m[roi_data_mean>-99].min()
-    u = roi_data_m[roi_data_mean>-99].max()
-    l = np.floor(l*200)/200.0
-    u = np.ceil(u*200)/200.0
-    
-    # Make sure the colorbar is centered
-    if l**2 < u **2:
-        l = u*-1
-    else:
-        u = l*-1
-    
-    brain.add_data(vtx_data_m,
-                    l, 
-                    u,
-                    thresh = -98,
-                    colormap="PuOr_r",
-                    alpha=.8)
-    
-    views_list = [ 'medial', 'lateral' ]
-    prefix = '_'.join([measure, hemi, surface, 'm'])
-    brain.save_imageset(prefix = os.path.join(fs_rois_dir, prefix),
-                        views = views_list, 
-                        colorbar = range(len(views_list)) )
-    
-    '''
-    
-    ### SIGNIFICANCE w AGE
-    brain = Brain(subject_id, hemi, surface,
-                  subjects_dir = subjects_dir,
-                  config_opts=dict(background="white"))
-
-    l = roi_data_p[roi_data_mean>-99].min()
-    u = roi_data_p[roi_data_mean>-99].max()
-    l = np.floor(l*20)/20.0
-    u = np.ceil(u*20)/20.0
-    
-    brain.add_data(vtx_data_p,
-                    0.95, 
-                    1.0,
-                    thresh = 0.95,
-                    colormap="autumn",
-                    alpha=.8)
-    
-    views_list = [ 'medial', 'lateral' ]
-    prefix = '_'.join([measure, hemi, surface, 'p'])
-    brain.save_imageset(prefix = os.path.join(fs_rois_dir, seg, prefix),
-                        views = views_list, 
-                        colorbar = range(len(views_list)) )
-                       
-for measure, surface in it.product(measure_list, surface_list):
-    #combine_pngs(measure, surface, 'mean', fs_rois_dir)
-    combine_pngs(measure, surface, 'r', os.path.join(fs_rois_dir, seg))
-    combine_pngs(measure, surface, 'p', os.path.join(fs_rois_dir, seg))
-    #combine_pngs(measure, surface, 'm', fs_rois_dir)
+#============================================================================= 
+# COMBINE THE IMAGES
+#============================================================================= 
+for surface in surface_list:
+    combine_pngs(measure, surface, output_dir)
+    combine_pngs(measure, surface, output_dir)
