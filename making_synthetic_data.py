8f4df0ff5534fafd7647e042b27fa62c9b272158
Kirstie Whitaker <kw401@cam.ac.uk>
2015-04-15 10:31:32 +0100

    updated to match the original data

--- making_synthetic_data.py
+++ making_synthetic_data.py
@@ -3,6 +3,7 @@
 '''
 Making synthetic data to test the cortical projection accuracy
 '''
+
 import nibabel as nib
 import numpy as np
 import os
@@ -13,13 +14,14 @@ if len(sys.argv[1]) == 1:
     print 'USAGE: making_synthetic_data.py <surfer_dir>'
     print '    eg: making_synthetic_data.py /home/kw401/UCHANGE/INTERIM_ANALYSIS/SUB_DATA/10736/SURFER/MRI0/'
     sys.exit()
-    
+
 # Get the surfer directory from the arguments
 surfer_dir = sys.argv[1]
 
 # Define the input and output filenames
 aparc_filename = os.path.join(surfer_dir, 'parcellation', '500.aparc.nii.gz')
-synth_filename = os.path.join(surfer_dir, 'synthetic', 'synthetic.nii.gz')
+MT_filename = os.path.join(surfer_dir, 'mri', 'MT.mgz')
+synth_filename = os.path.join(surfer_dir, 'mri', 'synthetic.mgz')
 
 # Check that the input file name exists
 if not os.path.isfile(aparc_filename):
@@ -28,29 +30,37 @@ if not os.path.isfile(aparc_filename):
     print '    eg: making_synthetic_data.py /home/kw401/UCHANGE/INTERIM_ANALYSIS/SUB_DATA/10736/SURFER/MRI0/'
     sys.exit()
 
-# Make the output directory if it doesn't yet exist
-if not os.path.isdir(os.path.join(surfer_dir, 'synthetic')):
-    os.mkdir(os.path.join(surfer_dir, 'synthetic'))
-
-# Load the parcellation file    
-img = nib.load(aparc_filename)
+# Load the parcellation and MT files
+parc_img = nib.load(aparc_filename)
+MT_img = nib.load(MT_filename)
 
 # Get the data
-data = img.get_data()
+parc_data = parc_img.get_data()
+parc_data=parc_data[...,0]
+MT_data = MT_img.get_data()
 
-# Assign grey matter voxels to have a mean of 900
-# and a standard deviation of 50
-data[data>1000] = np.random.normal(900, 50, size=data[data>1000].shape)
+# Create a copy of the MT_data as the synth_data
+synth_data = np.zeros_like(MT_data)
 
-# Assign white matter voxels to have a mean of 1500
-# and a standard deviation of 75
-data[data==41] = np.random.normal(1500, 75, size=data[data==41].shape)
-data[data==2] = np.random.normal(1500, 75, size=data[data==2].shape)
-data[(data>200) & (data<250)] = np.random.normal(1500, 75, size=data[(data>200) & (data<250)].shape)
+# Figure out the grey and white matter means from the real data
+gm_mean = MT_data[parc_data>1000].mean()
+gm_std = MT_data[parc_data>1000].std()
+wm_mean = MT_data[(parc_data==41) + (parc_data==2)].mean()
+wm_std = MT_data[(parc_data==41) + (parc_data==2)].std()
 
-# Set all other values to 0
-data[data<300] = 0
+# Assign grey matter voxels to have a random value from the distribution
+# defined by their grey matter mean and standard deviations
+synth_data[parc_data>1000] = np.random.normal(gm_mean, gm_std, size=parc_data[parc_data>1000].shape)
+
+# Assign white matter voxels to have a random value from the distribution
+# defined by their white matter mean and standard deviations
+synth_data[parc_data==41] = np.random.normal(wm_mean, wm_std, size=parc_data[parc_data==41].shape)
+synth_data[parc_data==2] = np.random.normal(wm_mean, wm_std, size=parc_data[parc_data==2].shape)
+synth_data[(parc_data>200) & (parc_data<250)] = np.random.normal(wm_mean, 
+                                                                    wm_std, 
+                                                                    size=parc_data[(parc_data>200)
+                                                                                    & (parc_data<250)].shape)
 
 # Now save the file
-img_synth = nib.Nifti1Image(data, img.get_affine())
-img_synth.to_filename(synth_filename)
+synth_img = nib.MGHImage(synth_data, MT_img.get_affine())
+synth_img.to_filename(synth_filename)
