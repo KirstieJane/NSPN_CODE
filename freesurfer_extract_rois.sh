84b31e03d1f5be1e0c4375da910a629572755ffe
Kirstie Whitaker <kw401@cam.ac.uk>
2015-05-14 08:39:08 +0100

    put all the options back

--- freesurfer_extract_rois.sh
+++ freesurfer_extract_rois.sh
@@ -171,69 +171,69 @@ done
 #     500.aparc_cortical_expanded_consecutive_WMoverlap
 #=============================================================================
 
-#for measure in R1 MT R2s A FA MD MO L1 L23 sse; do
-for measure in MT; do
+for measure in R1 MT R2s A FA MD MO L1 L23 sse; do
+#for measure in MT; do
     if [[ -f ${surfer_dir}/mri/${measure}.mgz ]]; then
 
-        # #=== wmparc
-        # if [[ ! -f ${surfer_dir}/stats/${measure}_wmparc.stats ]]; then
-            # mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
-                         # --seg ${surfer_dir}/mri/wmparc.mgz \
-                         # --ctab ${FREESURFER_HOME}/WMParcStatsLUT.txt \
-                         # --sum ${surfer_dir}/stats/${measure}_wmparc.stats \
-                         # --pv ${surfer_dir}/mri/norm.mgz
-        # fi
+        #=== wmparc
+        if [[ ! -f ${surfer_dir}/stats/${measure}_wmparc.stats ]]; then
+            mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
+                         --seg ${surfer_dir}/mri/wmparc.mgz \
+                         --ctab ${FREESURFER_HOME}/WMParcStatsLUT.txt \
+                         --sum ${surfer_dir}/stats/${measure}_wmparc.stats \
+                         --pv ${surfer_dir}/mri/norm.mgz
+        fi
         
-        # #=== aseg
-        # if [[ ! -f ${surfer_dir}/stats/${measure}_aseg.stats ]]; then
-            # mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
-                         # --seg ${surfer_dir}/mri/aseg.mgz \
-                         # --sum ${surfer_dir}/stats/${measure}_aseg.stats \
-                         # --pv ${surfer_dir}/mri/norm.mgz \
-                         # --ctab ${FREESURFER_HOME}/ASegStatsLUT.txt 
-        # fi
+        #=== aseg
+        if [[ ! -f ${surfer_dir}/stats/${measure}_aseg.stats ]]; then
+            mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
+                         --seg ${surfer_dir}/mri/aseg.mgz \
+                         --sum ${surfer_dir}/stats/${measure}_aseg.stats \
+                         --pv ${surfer_dir}/mri/norm.mgz \
+                         --ctab ${FREESURFER_HOME}/ASegStatsLUT.txt 
+        fi
         
-        # #=== lobesStrict
-        # if [[ ! -f ${surfer_dir}/stats/${measure}_lobesStrict.stats ]]; then
-            # mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
-                         # --seg ${surfer_dir}/mri/lobes+aseg.mgz \
-                         # --sum ${surfer_dir}/stats/${measure}_lobesStrict.stats \
-                         # --pv ${surfer_dir}/mri/norm.mgz \
-                         # --ctab ${lobes_ctab}
+        #=== lobesStrict
+        if [[ ! -f ${surfer_dir}/stats/${measure}_lobesStrict.stats ]]; then
+            mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
+                         --seg ${surfer_dir}/mri/lobes+aseg.mgz \
+                         --sum ${surfer_dir}/stats/${measure}_lobesStrict.stats \
+                         --pv ${surfer_dir}/mri/norm.mgz \
+                         --ctab ${lobes_ctab}
         
-        # fi
+        fi
         
-        # #=== 500.aparc_cortical_consecutive.nii.gz
-        # # Extract measures from the cortical regions in the 500 parcellation
-        # if [[ ! -f ${surfer_dir}/stats/${measure}_500cortConsec.stats 
-                # && -f ${surfer_dir}/parcellation/500.aparc_cortical_consecutive.nii.gz ]]; then
-            # mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
-                         # --seg ${surfer_dir}/parcellation/500.aparc_cortical_consecutive.nii.gz  \
-                         # --sum ${surfer_dir}/stats/${measure}_500cortConsec.stats \
-                         # --pv ${surfer_dir}/mri/norm.mgz \
-                         # --ctab ${parc500_ctab}
-        # fi
+        #=== 500.aparc_cortical_consecutive.nii.gz
+        # Extract measures from the cortical regions in the 500 parcellation
+        if [[ ! -f ${surfer_dir}/stats/${measure}_500cortConsec.stats 
+                && -f ${surfer_dir}/parcellation/500.aparc_cortical_consecutive.nii.gz ]]; then
+            mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
+                         --seg ${surfer_dir}/parcellation/500.aparc_cortical_consecutive.nii.gz  \
+                         --sum ${surfer_dir}/stats/${measure}_500cortConsec.stats \
+                         --pv ${surfer_dir}/mri/norm.mgz \
+                         --ctab ${parc500_ctab}
+        fi
         
-        # #=== 500.aparc_cortical_expanded_consecutive_WMoverlap
-        # # Only run this if there is a 500 cortical parcellation
-        # if [[ ! -f ${surfer_dir}/stats/${measure}_500cortExpConsecWMoverlap.stats \
-                # && -f ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive.nii.gz ]]; then
+        #=== 500.aparc_cortical_expanded_consecutive_WMoverlap
+        # Only run this if there is a 500 cortical parcellation
+        if [[ ! -f ${surfer_dir}/stats/${measure}_500cortExpConsecWMoverlap.stats \
+                && -f ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive.nii.gz ]]; then
             
-            # # Create the overlap file if it doesn't already exist
-            # if [[ ! -f ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive_WMoverlap.nii.gz ]]; then
+            # Create the overlap file if it doesn't already exist
+            if [[ ! -f ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive_WMoverlap.nii.gz ]]; then
             
-                # fslmaths ${surfer_dir}/parcellation/500.aparc_whiteMatter.nii.gz \
-                            # -bin \
-                            # -mul ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive.nii.gz \
-                            # ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive_WMoverlap.nii.gz
-            # fi
+                fslmaths ${surfer_dir}/parcellation/500.aparc_whiteMatter.nii.gz \
+                            -bin \
+                            -mul ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive.nii.gz \
+                            ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive_WMoverlap.nii.gz
+            fi
             
-            # mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
-                         # --seg ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive_WMoverlap.nii.gz \
-                         # --sum ${surfer_dir}/stats/${measure}_500cortExpConsecWMoverlap.stats \
-                         # --pv ${surfer_dir}/mri/norm.mgz \
-                         # --ctab ${parc500_ctab}
-        # fi
+            mri_segstats --i ${surfer_dir}/mri/${measure}.mgz \
+                         --seg ${surfer_dir}/parcellation/500.aparc_cortical_expanded_consecutive_WMoverlap.nii.gz \
+                         --sum ${surfer_dir}/stats/${measure}_500cortExpConsecWMoverlap.stats \
+                         --pv ${surfer_dir}/mri/norm.mgz \
+                         --ctab ${parc500_ctab}
+        fi
         echo "===="
     else
         echo "${measure} file not transformed to Freesurfer space"
@@ -252,8 +252,8 @@ done
 # Loop over both left and right hemispheres
 for hemi in lh rh; do
     # Loop over parcellations
-    #for parc in aparc 500.aparc lobesStrict; do
-    for parc in 500.aparc; do
+    for parc in aparc 500.aparc lobesStrict; do
+    #for parc in 500.aparc; do
 
         # First extract just the thickness & curvature values
         if [[ ! -f ${surfer_dir}/stats/${hemi}.${parc}.stats \
@@ -265,13 +265,13 @@ for hemi in lh rh; do
         fi
         
         # Next loop through all the different MPM and DTI files
-        #for measure in R1 MT R2s A FA MD MO L1 L23 sse synthetic; do
-        for measure in MT; do
+        for measure in R1 MT R2s A FA MD MO L1 L23 sse synthetic; do
+        #for measure in MT; do
 
             # Loop through a bunch of different fractional depths 
             # from the white matter surface
-            # for frac in `seq -f %+02.2f -1 0.05 1`; do
-            for frac in `seq -f %+02.2f 0 0.1 1`; do
+            for frac in `seq -f %+02.2f -1 0.05 1`; do
+            #for frac in `seq -f %+02.2f 0 0.1 1`; do
 
                 # Project the values to the surface
                 if [[ ! -f ${surfer_dir}/surf/${hemi}.${measure}_projfrac${frac}.mgh ]]; then
@@ -330,7 +330,7 @@ for hemi in lh rh; do
             
             # Now loop through the different absolute depths
             # **from the grey/white matter boundary**
-            for dist in `seq -f %+02.2f -1 0.2 0`; do
+            for dist in `seq -f %+02.2f -2 0.2 0`; do
 
                 if [[ ! -f ${surfer_dir}/surf/${hemi}.${measure}_projdist${dist}_fromBoundary.mgh ]]; then
                 
