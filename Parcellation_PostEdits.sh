13a97d6864005cac4da8609e0659ecaa17c488d0
Kirstie Whitaker <kw401@cam.ac.uk>
2015-09-17 11:57:51 +0100

    easier to use for HPC

--- Parcellation_PostEdits.sh
+++ Parcellation_PostEdits.sh
@@ -9,17 +9,17 @@
 #================================================================
 
 #==============================================================================
-# USAGE: Parcellation_PostEdits.sh <data_dir>
+# USAGE: Parcellation_PostEdits.sh <study_dir> <sub>
 #==============================================================================
 function usage {
 
-    echo "USAGE: Parcellation_PostEdits.sh <study_dir> <sublist>"
+    echo "USAGE: Parcellation_PostEdits.sh <study_dir> <sub>"
     echo "       Where <study_dir> is the parent directory to"
     echo "       the SUB_DATA directory and expects to find"
     echo "       SUB_DATA inside it and then the standard"
-    echo "       NSPN directory structure and <sublist> is a text"
-    echo "       file containing the NSPN ids and should be inside"
-    echo "       <study_dir>"
+    echo "       NSPN directory structure and <sub> is an"
+    echo "       NSPN subject ID that corresponds to a folder in"
+    echo "       the SUB_DATA directory"
     echo ""
     echo "DESCRIPTION: This code creates the parcellation for each person"
     echo "             in their freesurfer space, and should be applied"
@@ -32,15 +32,15 @@ function usage {
 #=============================================================================
 
 study_dir=$1
-sublist=$2
+sub=$2
 
 if [[ ! -d ${study_dir} ]]; then
     echo "**** STUDY DIRECTORY does not exist ****"
     usage
 fi
 
-if [[ ! -f ${sublist} ]]; then
-    echo "**** SUBLIST does not exist ****"
+if [[ -z ${sub} ]]; then
+    echo "**** No subject ID given ****"
     usage
 fi
     
@@ -51,33 +51,31 @@ sub_data_dir=${study_dir}/SUB_DATA
 fsaverage_subid=fsaverageSubP
 
 #================================================================
-# Transform the fsaverage parcellations for each subject
-# in sublist
+# Transform the fsaverage parcellations
 #================================================================
-for sub in `cat ${sublist}`; do
-    
-    SUBJECTS_DIR=${sub_data_dir}
-    subjid=MRI0
-        
-    for hemi in lh rh; do
-        
-        # Transform the surface parcellation from fsaverage space 
-        # to indiviual native space
-        mri_surf2surf --srcsubject ${fsaverage_subid} \
-                        --sval-annot ${SUBJECTS_DIR}/${fsaverage_subid}/label/${hemi}.500.aparc \
-                        --trgsubject ${sub}/SURFER/${subjid} \
-                        --trgsurfval ${SUBJECTS_DIR}/${sub}/SURFER/${subjid}/label/${hemi}.500.aparc \
-                        --hemi ${hemi}
-	done
-    
-	# Transform indivual surface parcellation to individual volume parcellation
-	mkdir -p ${SUBJECTS_DIR}/${sub}/SURFER/${subjid}/parcellation/
-	mri_aparc2aseg --s ${sub}/SURFER/${subjid} \
-                    --o ${SUBJECTS_DIR}/${sub}/SURFER/${subjid}/parcellation/500.aparc.nii.gz \
-                    --annot 500.aparc \
-                    --rip-unknown \
-                    --hypo-as-wm
    
+SUBJECTS_DIR=${sub_data_dir}
+subjid=MRI0
+    
+for hemi in lh rh; do
+    
+    # Transform the surface parcellation from fsaverage space 
+    # to indiviual native space
+    mri_surf2surf --srcsubject ${fsaverage_subid} \
+                    --sval-annot ${SUBJECTS_DIR}/${fsaverage_subid}/label/${hemi}.500.aparc \
+                    --trgsubject ${sub}/SURFER/${subjid} \
+                    --trgsurfval ${SUBJECTS_DIR}/${sub}/SURFER/${subjid}/label/${hemi}.500.aparc \
+                    --hemi ${hemi}
+done
+
+# Transform indivual surface parcellation to individual volume parcellation
+mkdir -p ${SUBJECTS_DIR}/${sub}/SURFER/${subjid}/parcellation/
+mri_aparc2aseg --s ${sub}/SURFER/${subjid} \
+                --o ${SUBJECTS_DIR}/${sub}/SURFER/${subjid}/parcellation/500.aparc.nii.gz \
+                --annot 500.aparc \
+                --rip-unknown \
+                --hypo-as-wm
+
 done
 
 #================================================================
